44e7112eeb67424f810b82af8f65992791116a9e
Jps709 <jps7095@psu.edu>
2023-03-04 12:29:09 -0500

    Updoot 0.5

---
+++ src/card-list.js
@@ -0,0 +1,23 @@
+import { LitElement, html, css } from 'lit';
+import "@lrnwebcomponents/meme-maker/meme-maker.js";
+import "jps709-character-card/src/character-card.js";
+import "jmv-player-card/src/player-card.js";
+
+export class CardList extends LitElement {
+  static properties = {
+    
+  };
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return html`
+     <character-card></character-card>
+     <player-card></player-card>
+    `;
+  }
+}
+
+customElements.define('card-list', CardList);
